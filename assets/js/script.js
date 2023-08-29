5dbc2bd65f001f48e1f2716a3d07f5a7b86d9b29
Walter Hoxha <walter45hoxha@gmail.com>
2023-08-29 12:15:35 +0200

    aggiunto creazione buttons tramite js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //creo i buttons
+  classe = "btn btn-primary btn-lg";
+  document.querySelector(".btn-group").append(creaButton("+", classe, "+"));
+  document.querySelector(".btn-group").append(creaButton("-", classe, "-"));
+  document
+    .querySelector(".btn-group")
+    .append(creaButton("reset", classe, "Reset"));
   document.querySelector(".btn-group").onclick = counterClickCheck;
 
   function aumentaCounter(i) {
@@ -28,5 +35,27 @@ document.addEventListener("DOMContentLoaded", function () {
         newValue = azzeraCounter();
     }
     modificacounter(i, newValue);
+  }
+
+  function creaButton(val, classe, contenuto) {
+    let btn = document.createElement("button");
+    if (controllaVariabile(classe)) {
+      btn.className = classe;
+    }
+    if (controllaVariabile(val)) {
+      btn.setAttribute("value", val);
+    }
+    if (controllaVariabile(contenuto)) {
+      btn.innerHTML = contenuto;
+    }
+    return btn;
+  }
+
+  function controllaVariabile(variable) {
+    if (variable !== null && variable !== undefined) {
+      return true;
+    } else {
+      return false;
+    }
   }
 });
